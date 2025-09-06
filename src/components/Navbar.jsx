3efc8134278e4f5ea2043354958a441c2599b0fa
zaib <zaib@gmail>
2025-09-06 21:31:14 +0530

    signin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
    <>
     
-   <section className="border-2 flex justify-end space-x-4 h-[50px] text-[24px]">
+   <section className="border-2 flex justify-end space-x-4 h-[50px] text-[24px] bg-yellow-100">
      <Link to={"/"}>SignUp</Link>
     <Link to={"Signin"}>SignIn</Link>
    </section>
